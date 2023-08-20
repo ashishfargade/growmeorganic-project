bac1eb5e4b7c0047a9bafa3937dc0de91460f1b7
ashishfargade <fargadeashish@gmail.com>
2023-08-21 03:21:23 +0530

    Started with form

---
+++ src/components/Form.tsx
@@ -0,0 +1,118 @@
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
+type Person = {
+  name: string;
+  phone: number;
+  email: string;
+};
+
+export const Form: React.FC = () => {
+  const [person, setPerson] = useState<Person>({
+    name: "",
+    phone: 0,
+    email: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setPerson((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <form>
+        <Container sx={{ display: "flex", justifyContent: "center" }}>
+          <Paper
+            elevation={6}
+            sx={{
+              width: "60%",
+              alignSelf: "auto",
+              marginTop: 2,
+              py: 4,
+              display: "flex",
+              justifyContent: "center",
+              height: 400,
+            }}
+          >
+            <Stack
+              direction={"column"}
+              sx={{
+                width: "60%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Typography variant="h4" color="initial">
+                User Info
+              </Typography>
+
+              <TextField
+                id="name-input"
+                type="text"
+                label="Name"
+                variant="outlined"
+                name="name" //used in person object useState
+                required
+                onChange={handleChange}
+              />
+              <TextField
+                id="phone-input"
+                type="number"
+                label="Phone No."
+                variant="outlined"
+                name="phone"
+                required
+                onChange={handleChange}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PhoneIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                label="Email"
+                variant="outlined"
+                name="email"
+                fullWidth
+                onChange={handleChange}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <EmailIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+
+              <Button
+                type="submit"
+                onClick={() => {
+                  console.log(person);
+                }}
+                variant="outlined"
+                sx={{ width: 90, display: "flex", alignSelf: "center" }}
+              >
+                Submit
+              </Button>
+            </Stack>
+          </Paper>
+        </Container>
+      </form>
+    </>
+  );
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{bgcolor: 'black'}}>
+        <Toolbar>
+          <img src="src\assets\rocket-logo.png" />
+
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "left", px: 2 }}
+          >
+            Current Page
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
